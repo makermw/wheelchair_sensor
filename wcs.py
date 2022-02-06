551cb64c5e556bf49701fb9b4590ce9e40bd97fd
makermw <m.j.westmore@soton.ac.uk>
2022-02-06 16:29:59 +0000

    new file:   wcs.py

---
+++ wcs.py
@@ -0,0 +1,13 @@
+# Wheelchar reversing sensor using pi pico microcontroller
+# Matt Westmore, 2022.02.6
+# need to use micropython interpreter
+
+import machine
+import utime
+
+print("Wheelchair sensor starting")
+
+#initialise
+# forever loop
+# sense distance
+# beep depending on distance
